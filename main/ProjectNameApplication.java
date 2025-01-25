e892f2898af1d1e2e8002dd17c0428fb46b73a1c
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-01-25 19:41:23 +0530

    Update ProjectNameApplication.java

--- main/ProjectNameApplication.java
+++ main/ProjectNameApplication.java
@@ -1,1 +1,13 @@
+package com.example.projectname;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectNameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectNameApplication.class, args);
+    }
+}
 
