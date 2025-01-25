0a2746476524b8bb2d4e74c620c3b40c818fe915
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-01-25 19:40:00 +0530

    Update MyErrorDetails.java

--- main/Exceptions/MyErrorDetails.java
+++ main/Exceptions/MyErrorDetails.java
@@ -1,1 +1,13 @@
+package com.example.projectname.model;
+
+import java.time.LocalDateTime;
+
+public class MyErrorDetails {
+    
+    private LocalDateTime timeStamp;
+    private String details;
+    private String message;
+
+    // Constructors, getters, setters
+}
 
