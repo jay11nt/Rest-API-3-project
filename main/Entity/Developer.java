0b8337d21f01113d98e24e97c9d54bd2f0e4a809
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-01-25 17:44:23 +0530

    Update Developer.java

--- main/Entity/Developer.java
+++ main/Entity/Developer.java
@@ -1,1 +1,28 @@
+package com.example.projectname.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Developer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String skillSet;
+    private String level;     // Junior, Senior, Lead
+}
 
