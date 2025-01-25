f79b9d21f94b66a423365037e97113bc047bc9db
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-01-25 19:37:47 +0530

    Update GlobalExceptionHandler.java

--- main/Exceptions/GlobalExceptionHandler.java
+++ main/Exceptions/GlobalExceptionHandler.java
@@ -1,1 +1,20 @@
+package com.example.projectname.exception;
+
+import com.example.projectname.model.MyErrorDetails;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<MyErrorDetails> handleResourceNotFound(ResourceNotFoundException ex) {
+        MyErrorDetails errorDetails = new MyErrorDetails(LocalDateTime.now(), ex.getMessage(), "Resource Not Found");
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+}
 
