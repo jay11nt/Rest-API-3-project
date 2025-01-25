13f9f5115891972e8ab818d29b1b9898a096902b
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-01-25 19:31:44 +0530

    Update DeveloperRepository.java

--- main/Repository/DeveloperRepository.java
+++ main/Repository/DeveloperRepository.java
@@ -1,1 +1,8 @@
+package com.example.projectname.repository;
+
+import com.example.projectname.model.Developer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DeveloperRepository extends JpaRepository<Developer, Long> {
+}
 
