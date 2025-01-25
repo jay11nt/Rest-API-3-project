e3b75bdbe15a0fe09df2fea84ba3508fbf1f8e36
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-01-25 19:33:12 +0530

    Update TesterRepository.java

--- main/Repository/TesterRepository.java
+++ main/Repository/TesterRepository.java
@@ -1,1 +1,8 @@
+package com.example.projectname.repository;
+
+import com.example.projectname.model.Tester;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TesterRepository extends JpaRepository<Tester, Long> {
+}
 
