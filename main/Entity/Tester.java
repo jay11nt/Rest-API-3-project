0130f483bdb82abba15f978a2b46223807afaeee
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-01-25 17:44:52 +0530

    Create Tester.java

---
+++ main/Entity/Tester.java
@@ -0,0 +1,27 @@
+package com.example.projectname.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Tester {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String testToolExpertise;
+    private String level;       // Junior, Senior, Lead
+}
