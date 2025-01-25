5270200ad8f4e518017d2b1570b9d7b0a154f0f7
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-01-25 19:38:56 +0530

    Update ResourceNotFoundException.java

--- main/Exceptions/CustomException/ResourceNotFoundException.java
+++ main/Exceptions/CustomException/ResourceNotFoundException.java
@@ -1,1 +1,9 @@
+package com.example.projectname.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
 
